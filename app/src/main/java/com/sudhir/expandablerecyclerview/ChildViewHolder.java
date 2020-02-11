6616720c2b905f131704acc00154fc90e331cbfd
sudhirsingh <sudhirs745@gmail.com>
2020-02-12 01:01:25 +0530

    Initial commit

---
+++ app/src/main/java/com/sudhir/expandablerecyclerview/ChildViewHolder.java
@@ -0,0 +1,13 @@
+package com.sudhir.expandablerecyclerview;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class ChildViewHolder extends RecyclerView.ViewHolder {
+
+    public ChildViewHolder(View itemView) {
+        super(itemView);
+    }
+}
---
+++ app/src/main/java/com/sudhir/expandablerecyclerview/Movies.java
@@ -0,0 +1,14 @@
+package com.sudhir.expandablerecyclerview;
+
+public class Movies {
+
+    private String mName;
+
+    public Movies(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
